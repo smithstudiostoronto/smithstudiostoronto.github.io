23abebe3bafdc68e0e96640a0fbfe2fcd20fd6d3
Blake Smith <blakesmith@Blakes-MacBook-Pro.local>
2019-04-10 12:11:55 -0400

    version 2

---
+++ js/function.js
@@ -0,0 +1,15 @@
+/* 
+Version 2.3 
+File created by Alex Turnwall, www.turnwall.com
+This file simply combines some great plugins in one file for ease of use in the demo. 
+All plugins are the rights and licenses of their original authors, as indicated below:
+*/
+<script type="text/javascript">
+jQuery(document).ready(function($) {
+ 
+  // Hook up the current state to the nav bar
+  $('.page-nav').onePageNav();
+  
+});
+</script>
+
