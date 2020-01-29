bdc36c7195986d4530c0b06d2f2b1ba6234a072b
Blake J. Smith <blake@smithstudios.ca>
2020-01-29 02:33:24 -0500

    buildscript: determine dev or prod completed and tested

---
+++ buildScripts/build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#0
+function determine_prod_or_dev {
+    #fetch branch name
+    current_branch_raw="($(git branch | grep '*'))"
+    branch_string_length=${#current_branch_raw}
+    branch_string_length=$((branch_string_length-4))
+    current_branch=${current_branch_raw:3:branch_string_length}
+
+    #initiating publish
+    case "$current_branch" in
+        "dev")
+        echo "Initiating Dev Deploy..."
+        ;;
+        "prod")
+        echo "Initiating Prod Deploy..."
+        ;;
+        *)
+        echo Error: Check Current Branch or Directory
+        echo Note: Branch must be \"dev\" or \"prod\"
+    esac
+   # echo $branches
+    #case
+    #-Not under git -not in prod or dev -dev -prod
+    # git branch | grep '*' > resources/current_branch.txt
+    # IFS=' ' read -ar split_branch <<< head -n 1 resources/current_branch.txt
+    # echo "${split_branch[1]}"
+    #echo $splitfirstline
+}
+
+determine_prod_or_dev
---
+++ buildScripts/dev_or_prod.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+int main (int argc,char** argv ){
+
+	cout<<argc<<endl;
+	for(unsigned i=0; i < argc ; i++){
+		cout<<argv[2]<<endl;
+
+	}
+	return 0;
+
+
+}
